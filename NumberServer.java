60994444decdccd7766336f1d984de10b5280bf9
wsx2021 <80902225+wsx2021@users.noreply.github.com>
2023-01-18 12:47:10 -0800

    Update NumberServer.java

--- NumberServer.java
+++ NumberServer.java
@@ -1,11 +1,15 @@
 import java.io.IOException;
 import java.net.URI;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 class Handler implements URLHandler {
     // The one bit of state on the server: a number that will be manipulated by
     // various requests.
     int num = 0;
-
+    List<String> stringList =  new ArrayList<String>();
     public String handleRequest(URI url) {
         if (url.getPath().equals("/")) {
             return String.format("Shixuan's Number: %d", num);
@@ -20,6 +24,17 @@ class Handler implements URLHandler {
                     num += Integer.parseInt(parameters[1]);
                     return String.format("Number increased by %s! It's now %d", parameters[1], num);
                 }
+                else {
+                    System.out.println("StringList");
+                    stringList.add(parameters[0]);
+                    System.out.println(parameters[0]);
+                    String[] strArray = new String[stringList.size()];
+		strArray = stringList.toArray(strArray);
+		System.out.println(Arrays.toString(strArray));
+
+                    return Arrays.toString(strArray);
+                }
+                
             }
             return "404 Not Found!";
         }
